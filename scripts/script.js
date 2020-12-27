69971b8a4a436e3b399af41a8711d392ed82f27a
ccchhaannttaall <62564113+ccchhaannttaall@users.noreply.github.com>
2020-12-27 21:00:35 +0100

    Css aanpassingen gedaan aan magazine kant

--- scripts/script.js
+++ scripts/script.js
@@ -2,16 +2,25 @@
 // van Chantal Valk
 var coll = document.getElementsByClassName("collapsible");
 
+//https://css-tricks.com/working-with-javascript-media-queries/
+var mediaQuery = window.matchMedia('(min-width: 30em)');
+
 for (var i = 0; i < coll.length; i++) {
-    coll[i].addEventListener("click", function() {
-        this.classList.toggle("active");
-        var content = this.nextElementSibling;
-        if (content.style.display === "block") {
-            content.style.display = "none";
-        } else {
-            content.style.display = "block";
-        }
-    });
+    if (mediaQuery.matches) {
+        coll[i].classList.toggle("active");
+        var content = coll[i].nextElementSibling;
+        content.style.display = "flex";
+    } else {
+        coll[i].addEventListener("click", function () {
+            this.classList.toggle("active");
+            var content = this.nextElementSibling;
+            if (content.style.display === "flex") {
+                content.style.display = "none";
+            } else {
+                content.style.display = "flex";
+            }
+        });
+    }
 }
 
 /* menu klikbaar maken*/
