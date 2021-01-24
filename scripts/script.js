29c831120e8903261cfd3055e712d33d521f7fde
ccchhaannttaall <62564113+ccchhaannttaall@users.noreply.github.com>
2021-01-24 20:27:48 +0100

    pointless...
    life is

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,9 @@
 var coll = document.getElementsByClassName("collapsible");
 
 //https://css-tricks.com/working-with-javascript-media-queries/
+// Deze mediaQuery is nodig omdat op mobiel de lijst standaard gesloten is en moet kunnen worden open en dicht geklapt.
+// Op het grote scherm moet deze altijd open zijn weergegeven. Dit kan dus niet enkel met css worden opgelost. Ik heb dit wel geprobeerd.
+// Deze javascript interactie had in overleg met J. Kapritsias niet uitgewerkt hoeven worden dit valt niet binnen de scope van de opdracht.
 var mediaQuery = window.matchMedia('(min-width: 30em)');
 
 for (var i = 0; i < coll.length; i++) {
@@ -23,8 +26,14 @@ for (var i = 0; i < coll.length; i++) {
     }
 }
 
+
 /* menu klikbaar maken*/
 var deButton = document.querySelector(".menuActief");
+
+var menuCloseButton = document.querySelector("aside button");
+menuCloseButton.addEventListener("click", function () {
+    doeFormHeenEnWeer();
+});
 
 function doeFormHeenEnWeer(){
     let hetFormulier = document.querySelector("aside");
